a2e3c64dab6bd59cc7ec74d97ccc4946557bdfdc
Adenike <adeyinkaadenike992@gmail.com>
2024-05-20 14:58:54 +0100

    commited

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,194 +1,204 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
-import { motion, useInView } from 'framer-motion';
-import phone from '../phone.svg'
+import React from "react";
+import styled from "styled-components";
 
-const variants = {
-  initial: {
-    y: 50,
-    opacity: 0
-  },
-  animate: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      duration: 0.5,
-      staggerChildren: 0.1,
+import { FaGithub, FaWhatsapp, FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+
+import { CgMail } from "react-icons/cg";
+
+
+
+
+const HeroSEctionStyles = styled.div`
+  padding-top: calc(var(--header-height));
+  .container {
+    padding: 5rem 1rem;
+
+    h1 {
+      font-size: 5rem;
+      
+      color: white;
+      font-weight: "bold";
+      font-style: unset;
+      font-family: "Inter", sans-serif;
+      /* text-align: center; */
+      
     }
-  }
-};
+    h5{
+        color: white; 
+        /* text-align: center; */
+        font-size: 1.6rem;
+        font-family: "Inter", sans-serif;
+        font-weight: 100;
+        letter-spacing: .2rem;
 
-const Section = styled.section`
-  padding: 0;
 
-  .contact {
-    height: 100%;
-    max-width: 1366px;
-    margin: auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    .textContainer {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      gap: 30px;
-
-      h1 {
-        font-size: 5rem;
-        line-height: 5rem;
-        color: white;
-      }
-
-      span {
-        font-weight: 300;
-        color: white;
-      }
-
-      h2 {
-        color: white;
-      }
-
-      h1, h2, span {
-        color: white;
-      }
     }
-
-    .formContainer {
-      flex: 1;
-      display: flex;
-      flex-direction: column;
-      gap: 20px;
-      position: relative;
-
-      .svg{
-        width: 100%;
-        height: 100%;
-        /* background-color: red; */
-        position: absolute;
-        margin: auto;
-        stroke: orange;
-
-
-        img{
-          width: 100%;
-          height: 100%;
-         
-        }
-      }
-
-      form {
+    .Acon {
+      width: 20px;
+      height: 20px;
+      color: white;
+      /* margin: 0px 10px; */
+      margin-right: 10px;
+    }
+    a{
+        
         display: flex;
-        flex-direction: column;
-        gap: 10px;
 
-        input, textarea {
-          padding: 20px;
-          margin-bottom: 10px;
-          border: 1px solid white;
-          border-radius: 5px;
-          color: white;
-          width: 100%;
-          background-color: transparent;
-        }
+        align-items: center;
+        flex-direction: row;
+        font-family: "Inter", sans-serif;
+       
+    }
 
-        button {
-          padding: 20px;
-          background-color: red;
-          color: white;
-          border: none;
-          border-radius: 5px;
-          cursor: pointer;
-
-          &:hover {
-            background-color: darkred;
-          }
-        }
-      }
+    span{
+      border-bottom: 2px solid red;
+       display: inline-block; 
+       padding: 5px;
+       margin-top: 1rem;
+       margin-bottom: 2rem;
+    }
+    h4{
+        color:white;  
+        font-size: 1.5rem;
+    }
+    .hr {
+      color: red;
+    }
+    .icon{
+      display: flex;
     }
   }
+  .scroll {
+    display: flex;
+    justify-content: flex-end;
+  
+    padding: 0rem 10rem;
+    cursor: pointer;
+    color:white;
+        font-size: 1.5rem;
+       
+        
+  }
+  .follow{
+     color: white;
+        font-size: 1.2rem;
+        font-weight: 100;
+        font-family: "Inter", sans-serif;
+        margin-bottom: 2rem;
+
+       
+  }
+
+ 
+
+  @media only screen and (max-width: 1200px) {
+    .container {
+      padding: 1.5rem 1rem;
+      h1 {
+        font-size: 2.6rem;
+        
+      }
+      h5{
+        font-size: 1rem;
+        font-weight: 600;
+      }
+
+    }
+    .scroll{
+        display: none;
+    }
+    
+  }
+  @media only screen and (max-width: 500px) {
+    .container {
+      padding: 1.5rem 1rem;
+
+      h1{
+        font-size: 2rem;
+      }
+      h5{
+        font-size: .8rem;
+        font-weight: 600;
+      }
+    
+    }
+  }
+
 `;
 
-const Container = styled.div`
-  margin: 0 auto;
-  padding: 0 3rem;
-  max-width: 1200px;
-`;
 
-const Loader = styled.div`
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: #000; /* or any color that matches your theme */
-  color: white;
-  font-size: 2rem;
-`;
 
-function Contact() {
-  const ref = useRef();
-  const isInView = useInView(ref, {margin:'-100px'})
-  const [isLoading, setIsLoading] = useState(true);
+  
 
-//   useEffect(() => {
-//     // Simulate loading time
-//     const timer = setTimeout(() => {
-//       setIsLoading(false);
-//     }, 2000); // Adjust the delay as needed
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   if (isLoading) {
-//     return <Loader>Loading...</Loader>;
-//   }
-
+function HeroSection() {
+  
   return (
-    <Section>
-      <Container>
-        <motion.div className="contact" variants={variants} initial="initial" whileInView="animate" ref={ref}>
-          <motion.div className="textContainer" variants={variants}>
-            <motion.h1 variants={variants}>Let's Work <br /> together</motion.h1>
-            <motion.div className="item" variants={variants}>
-              <h2>Mail</h2>
-              <span>adeyinkaadenike992@gmail.com</span>
-            </motion.div>
-            <motion.div className="item" variants={variants}>
-              <h2>github</h2>
-              <span>github.com/adenike005</span>
-            </motion.div>
-            <motion.div className="item" variants={variants}>
-              <h2>Phone</h2>
-              <span>+234-8161215777</span>
-            </motion.div>
-          </motion.div>
-          <div className="formContainer">
-          <motion.div className="svg" initial={{opacity:1}} whileInView={{opacity:0}} transition={{duration: 1, delay: 3}}>
-          <motion.img src={phone} alt='phone-icon' initial={{pathLength: 0}} animate={isInView &&{pathLength:1}} transition={{duration: 3}}/>
-          </motion.div>
-            {/* <svg>
-                <path
-                strokeWidth={0.2}
-                fill='none'
-                d
-                />
-            </svg> */}
+    
+    <HeroSEctionStyles id="contact">
+      <div className="container">
+ 
 
-            
-            <motion.form  initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration: 1, delay: 3}}>
+        <h1>
+        Interested to work with me?
+        </h1>
+        <h5>I’m currently looking to join a team of creative designers and developers.</h5>
 
-              <input type="text" required placeholder="Name" />
-              <input type="email" required placeholder="Email" />
-              <textarea rows={8} placeholder="Message" />
-              <button type="submit">Submit</button>
-            </motion.form>
-          </div>
-        </motion.div>
-      </Container>
-    </Section>
+       
+            <span>
+
+          <a
+            href="mailto:adeyinkaadenike992@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CgMail className="Acon" />
+             <div><h4>Send me an e-mail</h4></div>
+          </a>
+            </span>
+
+          <h6 className="follow">Follow me</h6>
+      
+          <div className="icon">
+          <a href="https://github.com/adenike005" target="__blank">
+            <FaGithub size={20} className="Acon" />
+          </a>
+          <a
+            href="https://wa.me/08161215777"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaWhatsapp className="Acon" />
+          </a>
+
+          <a
+            href="https://www.linkedin.com/in/adeyinka-adenike-6875a4257/"
+            target="_blank"
+          >
+            <FaLinkedinIn className="Acon" />
+          </a>
+          <a
+            href="https://www.instagram.com/adeyinka1853"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaInstagram className="Acon" />
+          </a>
+
+          <a
+            href="mailto:adeyinkaadenike992@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <CgMail className="Acon" />
+          </a>
+        </div>
+       
+      </div>
+
+     
+    </HeroSEctionStyles>
   );
 }
 
-export default Contact;
-
+export default HeroSection;
